e7981b61a30752a1038a180e326b863b1eb3aa9b
cartory <xXcartoryXx@gmail.com>
2021-06-24 22:42:41 -0400

    rol crud generated --login

---
+++ app/Models/Rol.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Rol
+ *
+ * @property $id
+ * @property $nombre
+ *
+ * @property Usuario[] $usuarios
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
+class Rol extends Model
+{
+
+    protected $table = 'Rol';
+    
+    static $rules = [
+		'nombre' => 'required',
+    ];
+
+    protected $perPage = 20;
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['nombre'];
+
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function usuarios()
+    {
+        return $this->hasMany('App\Models\Usuario', 'Rolid', 'id');
+    }
+    
+
+}
